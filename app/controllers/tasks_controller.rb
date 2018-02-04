c085905401e83e2d0a03236559f1313f3b7554d5
daichi hagiwara <daichi.hagiwara@livesense.co.jp>
2018-02-04 12:18:33 +0000

    user_idに紐付いた投稿をフォームからできるように、creatのメソッドを定義

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  
+
   def index
     @tasks = Task.all
   end
@@ -13,13 +13,14 @@ class TasksController < ApplicationController
   end
 
   def create
-  @task = Task.new(task_params)
+  @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Taskが正常に作成されました'
-      redirect_to @task
+      redirect_to root_url
     else
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'Taskが作成されませんでした'
-      render :new
+      render 'toppages/index'
     end
   end
 
