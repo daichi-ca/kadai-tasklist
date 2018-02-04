3f350d00ca4b87b1495e3ac7f0ebb007a1023a3a
daichi hagiwara <daichi.hagiwara@livesense.co.jp>
2018-02-04 07:32:44 +0000

    rootでの表示ページをトップページにした

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,6 @@ Rails.application.routes.draw do
 
   get 'users/create'
 
-  root to: 'tasks#index'
-
   get 'signup', to: 'users#new'
   resources :tasks
   resources :users, only: [:index, :show, :new, :create]
