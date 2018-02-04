6290034cc7d5eee76750ad6922082973f29b234c
daichi hagiwara <daichi.hagiwara@livesense.co.jp>
2018-02-04 07:44:04 +0000

    ログイン機能を追加するためsessionのcontrollerを生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destory'
+
   root to: 'toppages#index'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   get 'users/index'
-
   get 'users/show'
-
   get 'users/new'
-
   get 'users/create'
 
   get 'signup', to: 'users#new'
