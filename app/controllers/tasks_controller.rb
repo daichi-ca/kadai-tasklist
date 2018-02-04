9ca0b395fd59d3da7cf4be2d200a3abce647177e
daichi hagiwara <daichi.hagiwara@livesense.co.jp>
2018-02-04 14:24:46 +0000

    update成功時にredirect先をtoppageにした

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only:[:destroy]
   
   def index
     @tasks = Task.all
@@ -32,7 +32,7 @@ class TasksController < ApplicationController
   def update
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = 'Task は更新されませんでした'
       render :edit
